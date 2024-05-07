71b61b1b99d038e81885cd723c4203b92d1614a0
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2024-05-07 12:31:45 +0200

    добавление комментариев и переименование переменных и функций

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,49 +1,58 @@
 const modals = () => {
-  function bindModal(triggerSeletor, modalSeletor, closeSeletor) {
-    const trigger = document.querySelectorAll(triggerSeletor)
+  // Функция для закрытия модального окна и возвращения прокрутки фона
+  const closeModal = modal => {
+    modal.style.display = "none"
+    document.body.style.overflow = ""
+  }
+
+  // Функция для отображения модального окна через определенное время
+  const showModalByTime = (selector, time) => {
+    setTimeout(() => {
+      const modal = document.querySelector(selector)
+      if (modal) {
+        modal.style.display = "block"
+        document.body.style.overflow = "hidden"
+      }
+    }, time)
+  }
+
+  // Функция для привязки событий открытия/закрытия модального окна к триггерам
+  const bindModalEvents = (triggerSeletor, modalSeletor, closeSeletor) => {
+    const triggers = document.querySelectorAll(triggerSeletor)
     const modal = document.querySelector(modalSeletor)
     const close = document.querySelector(closeSeletor)
 
-    trigger.forEach(item => {
+    // Проходимся по каждому триггеру и добавляем обработчик клика
+    triggers.forEach(item => {
       item.addEventListener("click", e => {
-        if (e.target) e.preventDefault()
+        e.preventDefault()
 
         modal.style.display = "block"
         document.body.style.overflow = "hidden"
-        // document.body.classList.add("modal-open")
       })
     })
 
-    close.addEventListener("click", () => {
-      modal.style.display = "none"
-      document.body.style.overflow = ""
-      // document.body.classList.remove("modal-open")
-    })
+    // Добавляем обработчик клика на кнопку закрытия
+    close.addEventListener("click", () => closeModal(modal))
 
-    modal.addEventListener("click", e => {
-      if (e.target === modal) {
-        modal.style.display = "none"
-        document.body.style.overflow = ""
-        // document.body.classList.remove("modal-open")
+    // Добавляем обработчик клика на фон модального окна для закрытия
+    modal.addEventListener("click", event => {
+      if (event.target === modal) {
+        closeModal(modal)
       }
     })
   }
 
-  function showModalBytime(selector, time) {
-    setTimeout(() => {
-      document.querySelector(selector).style.display = "block"
-      document.body.style.overflow = "hidden"
-    }, time)
-  }
-
-  bindModal(
+  // Привязываем события к триггерам для открытия модальных окон
+  bindModalEvents(
     ".popup_engineer_btn",
     ".popup_engineer",
     ".popup_engineer .popup_close"
   )
+  bindModalEvents(".phone_link", ".popup", ".popup .popup_close")
 
-  bindModal(".phone_link", ".popup", ".popup .popup_close")
-  showModalBytime(".popup", 60000)
+  // Показываем модальное окно после определенного времени
+  showModalByTime(".popup", 60000)
 }
 
 export default modals
