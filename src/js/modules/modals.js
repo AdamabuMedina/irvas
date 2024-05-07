ce825bdcd7296bbde0b82453f9cd9a31f806edcb
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2024-05-07 12:19:58 +0200

    реструктуризация модального окна, добавление допольнительного функционала

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,22 +1,47 @@
 const modals = () => {
-  function bindModal(trigger, modal, close) {
-    trigger.addEventListener("click", e => {
-      if (e.target) e.preventDefault()
+  function bindModal(triggerSeletor, modalSeletor, closeSeletor) {
+    const trigger = document.querySelectorAll(triggerSeletor)
+    const modal = document.querySelector(modalSeletor)
+    const close = document.querySelector(closeSeletor)
 
-      modal.style.display = "block"
-      document.body.style.overflow = "hidden"
+    trigger.forEach(item => {
+      item.addEventListener("click", e => {
+        if (e.target) e.preventDefault()
+
+        modal.style.display = "block"
+        document.body.style.overflow = "hidden"
+        // document.body.classList.add("modal-open")
+      })
     })
 
     close.addEventListener("click", () => {
       modal.style.display = "none"
       document.body.style.overflow = ""
+      // document.body.classList.remove("modal-open")
+    })
+
+    modal.addEventListener("click", e => {
+      if (e.target === modal) {
+        modal.style.display = "none"
+        document.body.style.overflow = ""
+        // document.body.classList.remove("modal-open")
+      }
     })
   }
-  const callEngineerBtn = document.querySelector(".popup_engineer_btn"),
-    modalEngineer = document.querySelector(".popup_engineer"),
-    modalEngineerClose = document.querySelector(".popup_engineer .popup_close")
 
-  bindModal(callEngineerBtn, modalEngineer, modalEngineerClose)
+  function showModalBytime(selector, time) {
+    setTimeout(() => {
+      document.querySelector("")
+    })
+  }
+
+  bindModal(
+    ".popup_engineer_btn",
+    ".popup_engineer",
+    ".popup_engineer .popup_close"
+  )
+
+  bindModal(".phone_link", ".popup", ".popup .popup_close")
 }
 
 export default modals
