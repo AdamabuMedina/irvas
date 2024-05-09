dfb1ae63b6773c2df2e1fc81966d3c2b5e758db5
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2024-05-09 12:03:00 +0200

    Расширение функциональности управления состоянием модального окна

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -2,9 +2,9 @@ import { checkNumInputs } from "./utils"
 
 const changeModalState = state => {
   const windowForm = document.querySelectorAll(".balcon_icons_img")
-  const windowWidth = document.querySelector("#width")
-  const windowHeight = document.querySelector("#height")
-  const windowType = document.querySelector("#view_type")
+  const windowWidth = document.querySelectorAll("#width")
+  const windowHeight = document.querySelectorAll("#height")
+  const windowType = document.querySelectorAll("#view_type")
   const windowProfile = document.querySelectorAll(".checkbox")
 
   // Проверка введенных значений ширины и высоты на числовой ввод
@@ -13,15 +13,37 @@ const changeModalState = state => {
 
   // Функция для привязки действий к элементам интерфейса
   const bindActionToElems = (event, elem, prop) => {
-    elem.forEach((item, index) => {
+    elem.forEach((item, i) => {
       item.addEventListener(event, () => {
-        state[prop] = index
+        switch (item.nodeName) {
+          case "SPAN":
+            state[prop] = i
+            break
+          case "INPUT":
+            if (item.getAttribute("type") === "checkbox") {
+              i === 0 ? (state[prop] = "Холодное") : (state[prop] = "Теплое")
+              elem.forEach((box, j) => {
+                box.checked = false
+                if (i === j) box.checked = true
+              })
+            } else {
+              state[prop] = item.value
+            }
+            break
+          case "SELECT":
+            state[prop] = item.value
+            break
+        }
         console.log(state)
       })
     })
   }
 
   bindActionToElems("click", windowForm, "form")
+  bindActionToElems("input", windowHeight, "height")
+  bindActionToElems("input", windowWidth, "widht")
+  bindActionToElems("change", windowType, "type")
+  bindActionToElems("change", windowProfile, "profile")
 }
 
 export default changeModalState
